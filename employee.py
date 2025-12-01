7682112a8e0f79fb106f980fbe62276db4852ca4
Sharad-Agnihotri <sharadagnihotri197@gmail.com>
2025-12-01 15:42:23 +0530

    Update employee.py

--- employee.py
+++ employee.py
@@ -1,6 +1,6 @@
 
 def employee_info(name, emp_id, department, salary):
-    result= (
+    result = (
         f"Employee Name: {name}\n"
         f"Employee ID: {emp_id}\n"
         f"Department: {department}\n"
